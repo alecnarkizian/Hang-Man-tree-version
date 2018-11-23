dc299962c11752222b5b6915079fd101ae381dd8
alecnarkizian <45223913+alecnarkizian@users.noreply.github.com>
2018-11-22 20:48:12 -0800

    Add files via upload

---
+++ Apple Pie/Apple Pie3/Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Apple Pie3
+//
+//  Created by Alec Narkizian on 10/2/17.
+//  Copyright © 2017 Alec Narkizian. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    mutating func playerGuessed(letter: Character){
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter){
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
---
+++ Apple Pie/Apple Pie3Tests/Apple_Pie3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Apple_Pie3Tests.swift
+//  Apple Pie3Tests
+//
+//  Created by Alec Narkizian on 10/2/17.
+//  Copyright © 2017 Alec Narkizian. All rights reserved.
+//
+
+import XCTest
+@testable import Apple_Pie3
+
+class Apple_Pie3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
